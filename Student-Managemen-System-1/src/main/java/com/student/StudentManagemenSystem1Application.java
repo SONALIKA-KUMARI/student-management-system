b3ca02f3fb7b6f3e5f14cdc04818dd5cccd7512c
sonalika <sonalikakumari700il@gmail.com>
2023-05-15 13:09:20 +0530

    first commit

---
+++ Student-Managemen-System-1/src/main/java/com/student/StudentManagemenSystem1Application.java
@@ -0,0 +1,14 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagemenSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagemenSystem1Application.class, args);
+		System.out.println("sonalika");
+	}
+
+}
