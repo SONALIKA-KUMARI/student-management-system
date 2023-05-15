c89f2d4004c805ad2ccc74c4fc570aa97dc80f4c
sonalika <sonalikakumari700il@gmail.com>
2023-05-15 20:20:46 +0530

    6 commit

--- Student-Managemen-System-1/src/main/java/com/student/service/StudentService.java
+++ Student-Managemen-System-1/src/main/java/com/student/service/StudentService.java
@@ -7,4 +7,8 @@ import com.student.entity.Student;
 public interface StudentService {
 	List<Student> getAllStudents();
 	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+   Student updateStudent(Student student);
+	
 }
--- Student-Managemen-System-1/src/main/java/com/student/service/impl/StudentServiceImpl.java
+++ Student-Managemen-System-1/src/main/java/com/student/service/impl/StudentServiceImpl.java
@@ -35,6 +35,22 @@ public class StudentServiceImpl implements StudentService {
 		return studentRepository.save(student);
 	}
 
+
+
+	@Override
+	public Student getStudentById(Long id) {
+		
+		return studentRepository.findById(id).get();
+	}
+
+
+
+	@Override
+	public Student updateStudent(Student student) {
+		
+		return studentRepository.save(student);
+	}
+
 	
 	
 }
